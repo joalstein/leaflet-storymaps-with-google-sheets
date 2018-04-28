2c8bd65918126388211d98fe7d97a5edae26682f
joalstein <38804094+joalstein@users.noreply.github.com>
2018-04-27 23:13:41 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/13xYVafghpnz7OxK2ijT_ArJT8iZoWkA9_53yWl8HbAk/edit#gid=0';
